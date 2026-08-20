34711809eb5e62b2cc0b0b0ec4e96acd8d54801b
haneef <alimuhammadhaneef3@gmail.com>
2026-08-21 00:52:35 +0500

    Add Todo Task App

---
+++ todo.js
@@ -0,0 +1,55 @@
+const input=document.querySelector("#todoInput");
+const addbtn=document.querySelector(".Add");
+const todolist=document.getElementById("todolist");
+
+
+
+addbtn.addEventListener('click',()=>{
+    const task=input.value.trim();
+    if(task===""){
+        return;
+    }
+    const li=document.createElement('li')
+    li.textContent=task;
+
+    const editbtn=document.createElement('button')
+    editbtn.textContent='Edit';
+
+    const deletebtn= document.createElement("button")
+    deletebtn.textContent='Delete';
+    li.appendChild(editbtn);
+
+    li.appendChild(deletebtn);
+    todolist.appendChild(li);
+    input.value= '';
+})
+    todolist.addEventListener('click',(event)=>{
+
+        if(event.target.textContent==='Edit'){
+            const li=event.target.parentElement;
+
+            const newTask= prompt("edit your task :",li.firstChild.textContent);
+
+            if(newTask!==null & newTask.trim()!==""){
+                li.firstChild.textContent=newTask.trim();
+            }
+            return;
+        }
+        if(event.target.tagName==='BUTTON'){
+            event.target.parentElement.remove();
+            return;
+        }
+        if(event.target.tagName==='LI'){
+            event.target.classElement.toggle('completed')
+        }
+});
+
+input.addEventListener("keydown", (event) => {
+
+   if(event.key==='Enter'){
+    addbtn.click();
+   }
+
+});
+
+
